7095f793d79b449c170c893fbac7999d927fc820
Ben Coleman <benc.uk@gmail.com>
2017-08-13 11:15:34 +0100

    Refactor to single server. Dev/prod mode aware and visible to user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'ANGULAR 4 DEMO';
+  env = environment;
 }
